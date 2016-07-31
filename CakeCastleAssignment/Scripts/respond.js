4b45cc466cc80d14968496a5cb7d500433770487
Mansi <mpatel.y@gmail.com>
2016-07-30 20:45:55 -0400

    completed menu page

--- CakeCastleAssignment/Scripts/respond.js
+++ CakeCastleAssignment/Scripts/respond.js
@@ -261,3 +261,26 @@ $(document).ready(function () {
     });
 });
 --*/
+
+/**----------------------------------------------------------cakes img----------------------------------------------------*/
+$(document).ready(function () {
+    $('img').on('click', function () {
+        $("#showImg").empty();
+        var image = $(this).attr("src");
+        $("#showImg").append("<img class='img-responsive' src='" + image + "' />")
+    })
+});
+
+/*---------------------------------------------------------index pages img-----------------------------------------------------*/
+function centerModal() {
+    $(this).css('display', 'block');
+    var $dialog = $(this).find(".modal-dialog");
+    var offset = ($(window).height() - $dialog.height()) / 2;
+    // Center modal vertically in window
+    $dialog.css("margin-top", offset);
+}
+
+$('.modal').on('show.bs.modal', centerModal);
+$(window).on("resize", function () {
+    $('.modal:visible').each(centerModal);
+});
