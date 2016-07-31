174db1ac0e8d27aef945467892533282114e58e8
Mansi <mpatel.y@gmail.com>
2016-07-30 23:04:14 -0400

    finished home page with styling

--- CakeCastleAssignment/Scripts/respond.js
+++ CakeCastleAssignment/Scripts/respond.js
@@ -249,29 +249,3 @@ $(window).on("resize", function () {
     $('.modal:visible').each(centerModal);
 });
 
-/*----------------------------------------------------carousal*/
-var $item = $('.carousel .item');
-var $wHeight = $(window).height();
-$item.eq(0).addClass('active');
-$item.height($wHeight);
-$item.addClass('full-screen');
-
-$('.carousel img').each(function () {
-    var $src = $(this).attr('src');
-    var $color = $(this).attr('data-color');
-    $(this).parent().css({
-        'background-image': 'url(' + $src + ')',
-        'background-color': $color
-    });
-    $(this).remove();
-});
-
-$(window).on('resize', function () {
-    $wHeight = $(window).height();
-    $item.height($wHeight);
-});
-
-$('.carousel').carousel({
-    interval: 6000,
-    pause: "false"
-});
