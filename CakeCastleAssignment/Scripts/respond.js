af0609ecf6e35f180a733bbd46ad2b57df8ca90a
Mansi <mpatel.y@gmail.com>
2016-07-30 22:04:35 -0400

    home page

--- CakeCastleAssignment/Scripts/respond.js
+++ CakeCastleAssignment/Scripts/respond.js
@@ -223,8 +223,33 @@
   }
 })(this);
 
-/**--------------------------------------------------------------CAROUSAL----------------------------------------------------*/
 
+
+
+/**----------------------------------------------------------cakes img----------------------------------------------------*/
+$(document).ready(function () {
+    $('img').on('click', function () {
+        $("#showImg").empty();
+        var image = $(this).attr("src");
+        $("#showImg").append("<img class='img-responsive' src='" + image + "' />")
+    })
+});
+
+/*---------------------------------------------------------index pages img-----------------------------------------------------*/
+function centerModal() {
+    $(this).css('display', 'block');
+    var $dialog = $(this).find(".modal-dialog");
+    var offset = ($(window).height() - $dialog.height()) / 2;
+    // Center modal vertically in window
+    $dialog.css("margin-top", offset);
+}
+
+$('.modal').on('show.bs.modal', centerModal);
+$(window).on("resize", function () {
+    $('.modal:visible').each(centerModal);
+});
+
+/*----------------------------------------------------carousal*/
 var $item = $('.carousel .item');
 var $wHeight = $(window).height();
 $item.eq(0).addClass('active');
@@ -249,38 +274,4 @@ $(window).on('resize', function () {
 $('.carousel').carousel({
     interval: 6000,
     pause: "false"
-});
-
-/**-------------------------------------------------------------------------------------------------------
-$(document).ready(function () {
-    //FANCYBOX
-    //https://github.com/fancyapps/fancyBox
-    $(".fancybox").fancybox({
-        openEffect: "none",
-        closeEffect: "none"
-    });
-});
---*/
-
-/**----------------------------------------------------------cakes img----------------------------------------------------*/
-$(document).ready(function () {
-    $('img').on('click', function () {
-        $("#showImg").empty();
-        var image = $(this).attr("src");
-        $("#showImg").append("<img class='img-responsive' src='" + image + "' />")
-    })
-});
-
-/*---------------------------------------------------------index pages img-----------------------------------------------------*/
-function centerModal() {
-    $(this).css('display', 'block');
-    var $dialog = $(this).find(".modal-dialog");
-    var offset = ($(window).height() - $dialog.height()) / 2;
-    // Center modal vertically in window
-    $dialog.css("margin-top", offset);
-}
-
-$('.modal').on('show.bs.modal', centerModal);
-$(window).on("resize", function () {
-    $('.modal:visible').each(centerModal);
 });
